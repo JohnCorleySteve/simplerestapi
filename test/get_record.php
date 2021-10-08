52ff51b0d60f24c2aa0dd41ee2b028f77610be32
JohnCorleySteve <70510365+JohnCorleySteve@users.noreply.github.com>
2021-10-08 23:49:01 +0530

    Update get_record.php

--- test/get_record.php
+++ test/get_record.php
@@ -1,7 +1,7 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://localhost/phptask/currencies");
+curl_setopt($ch, CURLOPT_URL, "http://amit.byethost12.com/clients/simplerestapi/currencies");
 curl_setopt( $ch, CURLOPT_HTTPHEADER, [
 	"http-authorization: Bearer kdjf4u50943f78emjfoidjfg9rkue0"
 ]);
