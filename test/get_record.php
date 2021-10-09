a93b6e03cafa8b2f8b812f254c6f0e962888fa81
JohnCorleySteve <70510365+JohnCorleySteve@users.noreply.github.com>
2021-10-09 12:22:24 +0530

    Update get_record.php

--- test/get_record.php
+++ test/get_record.php
@@ -1,9 +1,16 @@
 <?php
 
+if(!isset ($_GET['page']) ) {  
+    $page = 1;  
+} else {  
+    $page = $_GET['page'];  
+} 
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://amit.byethost12.com/clients/simplerestapi/currencies");
+
+curl_setopt($ch, CURLOPT_URL, "http://amit.byethost12.com/clients/simplerestapi/currencies/&page=".$page);
 curl_setopt( $ch, CURLOPT_HTTPHEADER, [
-	"http-authorization: Bearer kdjf4u50943f78emjfoidjfg9rkue0"
+	"http-authorization: Bearer 6565393035326565323132646265333665313030336234616266346339333333"
 ]);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
